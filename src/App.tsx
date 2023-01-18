e768685586491f6ed6923b70de34454dc35da18a
Julia Sanchez <2509juliasanchez@gmail.com>
2023-01-18 14:23:39 -0300

    feat: configuring files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Last Of Us</h1>
+      <p>A Game's Guide</p>
+    </div>
+  );
+}
+
+export default App;
